8afc33ce330a1d0958461ccebddf3a6456333b0a
hakenshi <fkdias025@gmail.com>
2024-08-26 19:46:47 -0300

    modified:   resources/views/reset-password.blade.php

--- resources/views/reset-password.blade.php
+++ resources/views/reset-password.blade.php
@@ -5,27 +5,24 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-{{--    @vite('resources/css/app.css')--}}
+    <link rel="stylesheet" href="{{asset('css/reset-password.css')}}">
 </head>
-<body class="bg-card-white-1/50">
-    <div  class="flex justify-center flex-col items-center p-5 gap-2 md:gap-5 text-center">
-
-        <div class="bg-white p-5 min-h-96 md:min-h-[40rem] w-11/12 flex flex-col items-center justify-evenly">
-
-            <div>
-                <img class="w-32 md:w-72 lg:w-80" src="{{ asset("logo-unifae-2021.png") }}" alt="logo unifae">
-            </div>
-
-        <h1 class="text-base md:text-2xl font-bold">Instruções para redefinir a senha</h1>
-           <div class="text-center flex flex-col gap-2">
-               <p>Olá, {{$data['user']}}. Parece que você esqueceu a sua senha.</p>
-               <p >Por favor clique no botão abaixo para redefini-la.</p>
-           </div>
-                <div class="flex flex-col items-center gap-4">
-                    <a class="bg-unifae-green-1 w-fit text-white p-3 rounded-md" href="{{route('reset-password', ['token' => $data['token']])}}">Redefinir Senha</a>
-                    <p class="text-sm"> O botão não funciona? <a href="{{route('reset-password', ['token' => $data['token']])}}" class="underline text-blue-500 visited:text-purple-800">Clique aqui</a></p>
-                </div>
+<body class="background-card">
+<div class="container">
+    <div class="content-container">
+        <div>
+            <img class="logo" src="{{ asset('logo-unifae-2021.png') }}" alt="logo unifae">
+        </div>
+        <h1 class="title">Instruções para redefinir a senha</h1>
+        <div class="message-container">
+            <p>Olá, {{$data['user']}}. Parece que você esqueceu a sua senha.</p>
+            <p>Por favor, clique no botão abaixo para redefini-la.</p>
+        </div>
+        <div class="button-container">
+            <a class="reset-button" href="{{route('reset-password', ['token' => $data['token']])}}">Redefinir Senha</a>
+            <p class="fallback-text">O botão não funciona? <a href="{{route('reset-password', ['token' => $data['token']])}}" class="fallback-link">Clique aqui</a></p>
         </div>
     </div>
+</div>
 </body>
 </html>
