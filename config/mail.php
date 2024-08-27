cb81888403abf9afd49b64ffeb187aa77b9f8d03
Nykolas Fornaziero <nyfornaziero@gmail.com>
2024-08-27 09:27:02 -0300

    teste mail send

--- config/mail.php
+++ config/mail.php
@@ -14,7 +14,7 @@ return [
     |
     */
 
-    'default' => env('MAIL_MAILER', 'log'),
+    'default' => env('MAIL_MAILER', 'smtp'),
 
     /*
     |--------------------------------------------------------------------------
@@ -45,7 +45,7 @@ return [
             'username' => env('MAIL_USERNAME'),
             'password' => env('MAIL_PASSWORD'),
             'timeout' => null,
-            'local_domain' => env('MAIL_EHLO_DOMAIN'),
+//            'local_domain' => env('MAIL_EHLO_DOMAIN'),
         ],
 
         'ses' => [
