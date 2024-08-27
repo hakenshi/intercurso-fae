29f64ad008b9d4e4bf64c783c6c2ab596c7aa552
hakenshi <fkdias025@gmail.com>
2024-08-27 15:32:33 -0300

    fixed blade email views and fixed email reset password

--- routes/web.php
+++ routes/web.php
@@ -5,7 +5,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('reset-password');
 });
 
 Route::get('/reset-password/{token}', function ($token){
