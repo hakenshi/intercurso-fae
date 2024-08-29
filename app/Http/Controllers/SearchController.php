ea26ac51d071249ab058e96b786068370f8c9c59
Nykolas Fornaziero <nyfornaziero@gmail.com>
2024-08-29 15:31:04 -0300

    ajuste where

--- app/Http/Controllers/SearchController.php
+++ app/Http/Controllers/SearchController.php
@@ -72,7 +72,7 @@ class SearchController extends Controller
     public function jogadores(Request $request)
     {
         $data = $request->input('value');
-        $jogadores = User::where('ra', 'LIKE', "%'" . $data . "'%")->get();
+        $jogadores = User::where('ra', 'LIKE', "%" . $data . "%")->get();
 
         return $this->search($data, $jogadores);
     }
